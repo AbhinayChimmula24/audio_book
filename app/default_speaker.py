32fb1f7ceaef2b3d2bfb8f1229224a2e9fac8187
AbhinayChimmula24 <159165424+AbhinayChimmula24@users.noreply.github.com>
2024-05-12 00:25:07 -0400

    added some text processing

--- app/default_speaker.py
+++ app/default_speaker.py
@@ -7,9 +7,13 @@ tts = TTS("xtts")
 
 def speech_generator():
     text = Extract_From_PDF()
+    # Open the file in read mode ('r')
+with open('/Users/achimmula/Documents/GitHub/audio_book/rd_pd.txt', 'r') as file:
+    contents = file.read()
+
     # print(text)
-    tts.tts_to_file( text=text,
-                    file_path="/Users/achimmula/Desktop/audio_books/simon_vauce.wav",
+    tts.tts_to_file( text=contents,
+                    file_path="/Users/achimmula/Desktop/audio_books/simon_vauce_rd_pd_l1.wav",
                     speaker_wav= "/Users/achimmula/Desktop/speakers/simon_vausce.wav",
                     language="en",
                     split_sentences=True
--- app/extract_from_pdf.py
+++ app/extract_from_pdf.py
@@ -1,19 +1,43 @@
 # import streamlit as st
 from PyPDF2 import PdfReader
+import re
 
 def Extract_From_PDF(): 
-    pdf_path = "/Users/achimmula/Desktop/books_to_convert/NIPS-2017-attention-is-all-you-need-Paper.pdf"
+    pdf_path = "/Users/achimmula/Desktop/books_to_convert/Rich Dad Poor Dad.pdf"
     if pdf_path:
         pdf = PdfReader(pdf_path)
         extracted_text = ''
         for page_num in range(len(pdf.pages)):
             page = pdf.pages[page_num]
             extracted_text += page.extract_text()
-            with open('/Users/achimmula/Desktop/extracted_text.txt', 'w') as f:
-                f.write(extracted_text)
     return extracted_text
     # in future increase the file size
     # take this text and pass it to the ollama api or to the speech generator 
 
+def text_processing(starting_text, ending_text=None):
+    text = Extract_From_PDF()
+    text = text.replace('\n', ' ')
+
+    text = re.sub(r'(\.) ', r'\1\n', text)
+    text = re.sub(r'(\?) ', r'\1\n', text)
+    text = re.sub(r'(\") ', r'\1\n', text)
+
+    parts = text.split(starting_text)
+
+    if len(parts) > 1:
+        
+        ending_parts = parts[1].split(ending_text)
+
+        if len(ending_parts) > 1:
+            selected_text = starting_text + ending_parts[0]
+        else:
+            selected_text = starting_text + parts[1]
+    else: 
+        selected_text = text
+
+        with open('/Users/achimmula/Desktop/starting_c1.txt', 'w') as f:
+            f.write(selected_text)
+        return selected_text
+    
 if __name__ == "__main__":
-    Extract_From_PDF()
+    text_processing("LESSON 1: THE RICH DON’T", "LESSON 2: WHY TEACH FINANCIAL LITERACY?")
